3af7980c707247566e5715a82b29f2addd596d89
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-02-05 16:23:34 -0800

    Add files via upload

--- option1.py
+++ option1.py
@@ -4,14 +4,17 @@ from turtle import *
 backgroundColor = input("Background color: ")
 
 turtleColor = input("Turtle color: ")
-turtleSize = input("Turtle size: ")
+turtleSize = int(input("Turtle size: "))
 turtleShape = input("Turtle shape: ")
+
+screenWidth = int(input("Screen width: "))
+screenHeight = int(input("Screen height: "))
 
 # turtle and window
 s = Screen()
 t = Turtle()
 
-s.setup(width=600, height=600)
+s.setup(width=screenWidth, height=screenHeight)
 s.bgcolor(backgroundColor)
 
 t.speed(0)
@@ -20,6 +23,28 @@ t.color(turtleColor)
 t.shapesize(turtleSize)
 
 # shape inputs
+def squareSpiral(amountSS):
+    for i in range(amountSS):
+        t.goto(0, screenHeight)
+        t.right(7)
+        t.goto(0, t.ycor * int(amountSS))
+        for i in range(4):
+            t.forward(100)
+            t.left(90)
 
+# actions
+print("===============================")
+print("||What shape should be drawn?||")
+print("||    Spiral                 ||")
+print("||    Plus                   ||")
+print("||    Octagon                ||")
+print("===============================")
+shapeAsk = input("Input: ")
+
+if shapeAsk == "spiral" or shapeAsk == "Spiral":
+    amountSS = int(input("Spiral amount: "))
+    squareSpiral(int(amountSS))
+else:
+    print("Please input a valid value")
 
 s.mainloop()
