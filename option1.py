9200df14664ac2296840d480f0dabf21e9d7ccde
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-02-05 23:41:42 -0800

    Add files via upload

--- option1.py
+++ option1.py
@@ -1,50 +1,151 @@
 from turtle import *
 
+Turtle().hideturtle()
+
 # variables
-backgroundColor = input("Background color: ")
-
-turtleColor = input("Turtle color: ")
-turtleSize = int(input("Turtle size: "))
-turtleShape = input("Turtle shape: ")
-
-screenWidth = int(input("Screen width: "))
-screenHeight = int(input("Screen height: "))
-
-# turtle and window
 s = Screen()
 t = Turtle()
 
+invisibleFString = "Invisible (recommended)"
+
+backgroundColor = input("Background color: ").lower().strip()
+while not backgroundColor:
+    print("Please input a valid value")
+    backgroundColor = input("Background color: ").lower().strip()
+
+turtleColor = input("Turtle color: ").lower().strip()
+while not turtleColor:
+    print("Please input a valid value")
+    turtleColor = input("Turtle color: ").lower().strip()
+
+while True:
+    try:
+        screenWidth = int(input("Screen width: "))
+        break
+    except ValueError:
+        print("Please input a valid value")
+
+while True:
+    try:
+        screenHeight = int(input("Screen height: "))
+        break
+    except ValueError:
+        print("Please input a valid value")
+
+while True:
+    try:
+        turtleSize = int(input("Turtle size: "))
+        break
+    except ValueError:
+        print("Please input a valid value")
+
+print("====================================")
+print("||    Arrow                       ||")
+print("||    Turtle                      ||")
+print("||    Circle                      ||")
+print("||    Square                      ||")
+print("||    Triangle                    ||")
+print("||    Classic                     ||")
+print("||                                ||")
+print(f"||    {invisibleFString:<27} ||")
+print("====================================")
+
+turtleShape = input("Turtle shape: ").lower().strip()
+while not turtleShape:
+    print("Please input a valid value")
+    turtleShape = input("Turtle shape: ").lower().strip()
+
+if turtleShape == "invisible":  # duran duran
+    t.hideturtle()
+else:
+    t.shape(turtleShape)
+
+# turtle and window
 s.setup(width=screenWidth, height=screenHeight)
 s.bgcolor(backgroundColor)
 
 t.speed(0)
-t.shape(turtleShape)
 t.color(turtleColor)
 t.shapesize(turtleSize)
 
 # shape inputs
-def squareSpiral(amountSS):
-    for i in range(amountSS):
-        t.goto(0, screenHeight)
-        t.right(7)
-        t.goto(0, t.ycor * int(amountSS))
-        for i in range(4):
-            t.forward(100)
+def squareSpiral(amount):
+    sideLength = 20
+    for i in range(amount):
+        for _ in range(4):
+            t.down()
+            t.forward(sideLength)
             t.left(90)
+            t.up()
+        t.right(7) 
+        sideLength += 10 
+
+def plusQuarter():
+    for _ in range(2):
+        t.forward(50)
+        t.left(90)
+        t.forward(100)
+        t.left(90)
+    t.forward(50)
+
+def plus():
+    for i in range(4):
+        plusQuarter()
+        t.right(90)
+
+def tiledHexagons():
+    for i in range(3):
+        for i in range(6):
+            t.forward(100)
+            t.left(60)
+        t.right(120)
 
 # actions
 print("===============================")
-print("||What shape should be drawn?||")
 print("||    Spiral                 ||")
 print("||    Plus                   ||")
-print("||    Octagon                ||")
+print("||    Tiled Hexagons         ||")
 print("===============================")
-shapeAsk = input("Input: ")
+shapeAsk = input("Shape: ").lower().strip()
 
-if shapeAsk == "spiral" or shapeAsk == "Spiral":
-    amountSS = int(input("Spiral amount: "))
-    squareSpiral(int(amountSS))
+if shapeAsk == "spiral":
+    amount = int(input("Spiral amount: "))
+    squareSpiral(amount)
+    if amount >= 101:
+        amount = 100
+if shapeAsk == "plus":
+    plus()
+if shapeAsk == "tiled hexagons":
+    tiledHexagons()
 else:
     print("Please input a valid value")
+
+print("===============================")
+print("|| Stats:                    ||")
+print("||    Shape:                 ||")
+print(f"||    {shapeAsk:<22} ||")
+print("||                           ||")
+if shapeAsk == "spiral":
+    print("||    Amount:                ||")
+    print(f"||    {amount:<22} ||")
+    print("||                           ||")
+print("||    Color:                 ||")
+print(f"||    {turtleColor:<22} ||")
+print("||                           ||")
+print("||    Window dimensions:     ||")
+print(f"||    {screenWidth} x {screenHeight:<16} ||")
+print("||                           ||")
+print("||    Background color:      ||")
+print(f"||    {backgroundColor:<22} ||")
+print("||                           ||")
+print("||    Turtle size:           ||")
+print(f"||    {turtleSize:<22} ||")
+print("||                           ||")
+print("||    Turtle shape:          ||")
+print(f"||    {turtleShape:<22} ||")
+print("||                           ||")
+print("||    Turtle color:          ||")
+print(f"||    {turtleColor:<22} ||")
+print("===============================")
 
 s.mainloop()
